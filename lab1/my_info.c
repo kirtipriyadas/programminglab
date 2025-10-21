3a887e525bbb40b87d210a5112dfeb2fc9c2865b
kirtipriyadas <kirtipriyadas@gmail.com>
2025-10-21 16:50:53 +0530

    Add files via upload

---
+++ lab1/my_info.c
@@ -0,0 +1,10 @@
+include<stdio.h>
+int main(){
+	printf("Name:Kirti Priya Das");
+	printf("Roll No.:MEB25070");
+	printf("Btech First Year");
+	printf("Mechanical Engineering");
+	printf("Tezpur University");
+	return 0;
+	
+}
